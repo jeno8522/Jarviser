3799de16e0b442aa02a00543099a145af646862f
kimtaehyun <taehyun01212@gmail.com>
2023-07-24 14:45:15 +0900

    ㅇinit

---
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/JarviserApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.jarviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarviserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarviserApplication.class, args);
+	}
+
+}
---
+++ jarviser_backend/jarviser/src/test/java/com/ssafy/jarviser/JarviserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.jarviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarviserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
