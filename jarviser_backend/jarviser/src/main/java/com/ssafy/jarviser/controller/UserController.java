e6bbc82dfccb9e105b0d85c4f0614d7125abcb91
kimtaehyun <taehyun01212@gmail.com>
2023-07-25 14:37:28 +0900

    회원가입 api 구현

---
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.ssafy.jarviser.controller;
+
+import com.ssafy.jarviser.domain.RequestUserDto;
+import com.ssafy.jarviser.service.UserService;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    @PostMapping("/signup")
+    public ResponseEntity<Map<String, Object>> signUp(@RequestBody RequestUserDto userInfo) {
+        log.debug("User............................regist user:" + userInfo);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        try {
+            userService.regist(userInfo);
+            resultMap.put("message", SUCCESS);
+            status = HttpStatus.ACCEPTED;
+        } catch (Exception e) {
+            log.error("회원가입 실패 : {}", e);
+            resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+}
---
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/domain/User.java
@@ -0,0 +1,40 @@
+package com.ssafy.jarviser.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String userId;
+
+    private String password;
+
+    private String name;
+
+    private String email;
+
+    private String provider;
+
+    private String providedId;
+
+    @Builder
+    public User(int id, String userId, String password, String name, String email
+            ,String provider, String providedId) {
+        this.id = id;
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.provider = provider;
+        this.providedId = providedId;
+    }
+}
