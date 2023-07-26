19aa7e534fc6ae93986eb773814e76b04df9480a
kimtaehyun <taehyun01212@gmail.com>
2023-07-26 10:34:14 +0900

    feat: JWT 토큰 provider 구현

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/config/SecurityConfig.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/config/SecurityConfig.java
@@ -1,21 +1,24 @@
 package com.ssafy.jarviser.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
-@Configuration
-@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableWebSecurity //default security setting을 모두 버리겠다.
 public class SecurityConfig {
 
-    // 해당 메서드의 리턴되는 오브젝트는 IoC로 등록해준다
+    // authenticationManager를 Bean 등록합니다.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
