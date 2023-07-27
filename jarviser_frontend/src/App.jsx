b91a25331f6034466732a36b0faa0bd6d5ca0721
MoonHongWoong <ansghddnd12@naver.com>
2023-07-27 16:45:09 +0900

    stash

--- jarviser_frontend/src/App.jsx
+++ jarviser_frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Signup from "./pages/Signup";
+import Main from "./pages/Main";
+import Reservation from "./pages/Reservation";
 
 function App() {
   return (
     <>
-    <div><h1>JARVISER HEADER</h1></div>
-   <Router>
-      <Routes>
-        <Route path ="/login" element={<Login />} />
-        <Route path ="/register" element={<Register />} />
-      </Routes>
-     </Router>
-     </>
+      <div>
+        <h1>JARVISER HEADER</h1>
+      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/reservation" element={<Reservation />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
