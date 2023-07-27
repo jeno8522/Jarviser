91f92f2072d9e56c7c6491c0107c9c68a345f469
김민우 <jerry142857@naver.com>
2023-07-27 16:27:51 +0900

    feat : [+]Login, Signup, Header, Footer

--- jarviser_frontend/src/App.jsx
+++ jarviser_frontend/src/App.jsx
@@ -1,16 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/molecules/Header";
+import Footer from "./components/molecules/Footer";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Register from "./pages/Signup";
+import Main from "./pages/Main";
+import UserMain from "./pages/UserMain";
+import MyPage from "./pages/MyPage";
+import MyCalendar from "./pages/MyCalendar";
+import MyReport from "./pages/MyReport";
 
 function App() {
   return (
     <>
     <div><h1>JARVISER HEADER</h1></div>
    <Router>
+    <Header/>
       <Routes>
+        <Route path ="/" element={<Main />} />
         <Route path ="/login" element={<Login />} />
         <Route path ="/register" element={<Register />} />
+        <Route path ="/userMain" element={<UserMain />} />
+        <Route path ="/mypage" element={<MyPage />} />
+        <Route path ="/mycalendar" element={<MyCalendar />} />
+        <Route path ="/myreport" element={<MyReport />} />
       </Routes>
+      <Footer/>
      </Router>
      </>
   );
