a3879a2b209c20b045db8634abd1a92d1dde16eb
kimtaehyun <taehyun01212@gmail.com>
2023-07-31 17:37:07 +0900

    test: 메일 발송 테스트 코드 작성 및 검증 완료

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/service/EmailService.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/service/EmailService.java
@@ -15,8 +15,10 @@ public class EmailService {
 
     public void sendEmail(){
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setSubject("안녕하세요, 국가공인 맞짱플랫폼 MAZZANG9입니다.");
-        message.setTo("taehyun01212@gmail.com");
-        message.setText("문홍웅님으로부터 맞짱신청이 도착하였습니다.\n도전에 응하시겠습니까?\nhttps://moeni.net/");
+        message.setSubject("안녕하세요, 국가공인 맞짱플랫폼 MAZZANGGO?입니다.");
+        message.setTo("dnddl9368@gmail.com");
+        message.setText("나현웅님으로부터 맞짱신청이 도착하였습니다.\n도전에 응하시겠습니까?\nhttps://m.cafe.daum.net/subdued20club/_rec");
+
+        javaMailSender.send(message);
     }
 }
