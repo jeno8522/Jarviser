d722f18c2f719290531cce3bcf3ef02c630dc574
nhwjik@gmail.com <nhwjik@gmail.com>
2023-08-01 17:49:53 +0900

    Feat: 오디오 전처리 개발중 (웹 버전 VAD 적용 1차 완료 후 webRTC 프론트에 적용 전 단계)

--- jarviser_frontend/src/Audio/test4/test_logic2.js
+++ jarviser_frontend/src/Audio/test4/test_logic2.js
@@ -1,36 +1,79 @@
-//VAD를 적용하기 전 단계의 통신을 재구현한 코드
+//VAD를 적용하고 있는 코드
+
+// import { VAD } from "./vad.js";
+
 let mediaRecorder;
 let recordedChunks = [];
 let index = 0;
+let vad;
 
-document.getElementById("start").addEventListener("click", function () {
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext();
+
+function startVAD(stream) {
   navigator.mediaDevices
     .getUserMedia({ audio: true })
     .then((stream) => {
-      mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.start();
-
-      mediaRecorder.addEventListener("dataavailable", function (e) {
-        if (e.data.size > 0) {
-          recordedChunks.push(e.data);
-        }
-      });
-
-      mediaRecorder.addEventListener("stop", function () {
-        let blob = new Blob(recordedChunks, { type: "audio/wav" });
-        recordedChunks = [];
-        sendAudio(blob);
-        index++;
-      });
+      console.log("startVAD");
+      var source = audioContext.createMediaStreamSource(stream);
+      var options = {
+        source: source,
+        voice_stop: function () {
+          console.log("voice_stop");
+          stopAudio();
+        },
+        voice_start: function () {
+          console.log("voice_start");
+          startAudio();
+        },
+      };
+      vad = window.VAD(options);
     })
     .catch((err) => console.log("getUserMedia() failed: ", err));
-});
+}
 
-document.getElementById("stop").addEventListener("click", function () {
+function startAudio() {
+  if (!mediaRecorder) {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
+
+        mediaRecorder.addEventListener("dataavailable", function (e) {
+          if (e.data.size > 0) {
+            recordedChunks.push(e.data);
+            console.log("pushing..");
+          }
+        });
+
+        mediaRecorder.addEventListener("stop", function () {
+          let blob = new Blob(recordedChunks, { type: "audio/wav" });
+          recordedChunks = [];
+          sendAudio(blob);
+          index++;
+        });
+      })
+      .catch((err) => console.log("getUserMedia() failed: ", err));
+  } else {
+    mediaRecorder.start();
+  }
+}
+
+function stopAudio() {
   if (mediaRecorder) {
     mediaRecorder.stop();
   }
+}
+
+document.getElementById("vad_start").addEventListener("click", () => {
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then((stream) => startVAD(stream))
+    .catch((err) => console.log("getUserMedia() failed: ", err));
 });
+document.getElementById("start").addEventListener("click", startAudio);
+document.getElementById("stop").addEventListener("click", stopAudio);
 
 async function sendAudio(blob) {
   try {
