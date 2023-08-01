121b996a99d2b78978586d6dc10501c5955bada2
kimtaehyun <taehyun01212@gmail.com>
2023-08-01 17:11:54 +0900

    refactor: JPA repository를 상속받은 인터페이스와 custom 인터페이스 분리

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/repository/ReservationRepositoryCustom.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/repository/ReservationRepositoryCustom.java
@@ -1,12 +1,9 @@
 package com.ssafy.jarviser.repository;
 
-import com.ssafy.jarviser.domain.Reservation;
 import com.ssafy.jarviser.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReservationRepositoryCustom {
-    List<User> findUsersByReservationId(Long reservationId);
+    List<User> getUsersForMeetingRoom (Long id);
 }
