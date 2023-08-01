9425d8d70e6bbcbad3cbbab89227abca7f43939a
taehyun <taehyun0121@naver.com>
2023-08-01 09:18:15 +0900

    feat: reservation, reservatedMeeting repository 추가(미완)

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/domain/Reservation.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/domain/Reservation.java
@@ -13,8 +13,9 @@ public class Reservation {
     @Column(name = "reservation_id")
     private long id;
 
-    @Column(name = "user_id")
-    private long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
