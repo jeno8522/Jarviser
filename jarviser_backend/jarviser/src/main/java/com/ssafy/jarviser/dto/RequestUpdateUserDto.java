6a10fc83c19ebff07f254da74bbbb0568c5fb160
wooseok <wooseok777777@gmail.com>
2023-08-04 12:44:17 +0900

    requestupdateuserdto @NoArgsConstructor 추가

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/dto/RequestUpdateUserDto.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/dto/RequestUpdateUserDto.java
@@ -3,9 +3,11 @@ package com.ssafy.jarviser.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RequestUpdateUserDto {
     private String password;
     private String name;
