ce50f0009a3ae886ae81decf73333a39ac159ddb
jeno8522 <jeno8522@gmail.com>
2023-08-04 15:03:07 +0900

    feat: openvidu 설정2

--- jarviser_frontend/src/pages/CreateMeeting.jsx
+++ jarviser_frontend/src/pages/CreateMeeting.jsx
@@ -11,34 +11,16 @@
 
 // export default CreateMeeting;
 
-import { useState } from "react";
+import {useState} from "react";
 import VideoRoomComponent from "../components/openvidu/VideoRoomComponent"; // 경로는 실제 파일 위치에 따라 수정하세요.
-import axios from "axios";
 
 const CreateMeeting = () => {
   const [userName, setUserName] = useState("1234");
-  const [sessionName, setSessionName] = useState("My Room");
+  const [roomName, setRoomName] = useState("My Room");
   const [showVideoRoom, setShowVideoRoom] = useState(false);
-  const headers = {
-    Authorization: `Bearer ${localStorage.getItem("access-token")}`,
-  };
-  const sendData = {
-    meetingName: roomName,
-  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    try {
-      axios.post("http://localhost:8081/meeting/create/meetingtest", sendData, {
-        headers,
-      });
-      console.log(headers);
-      console.log(sendData);
-      alert(JSON.stringify(sendData));
-
-      reset();
-    } catch (error) {
-      console.error("There was an error!", error);
-    }
     setShowVideoRoom(true); // 폼이 제출되었으므로 VideoRoomComponent를 보여줍니다.
   };
 
@@ -54,18 +36,17 @@ const CreateMeeting = () => {
           />
         </label>
         <label>
-          Session Name:
+          Room Name:
           <input
             type="text"
-            value={sessionName}
-            onChange={(event) => setSessionName(event.target.value)}
+            value={roomName}
+            onChange={(event) => setRoomName(event.target.value)}
           />
         </label>
         <input type="submit" value="Submit" />
       </form>
       {showVideoRoom && (
-        // <VideoRoomComponent userName={userName} sessionName={sessionName} />
-
+        // <VideoRoomComponent userName={userName} sessionName={roomName} />
         <VideoRoomComponent />
       )}
     </div>
