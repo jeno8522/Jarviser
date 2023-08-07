b1e0306aefc69b202084a3aa4c64bdd918872c1b
jeno8522 <jeno8522@gmail.com>
2023-08-07 10:16:08 +0900

    feat: 미팅방 생성 시 자동생성 된 해쉬 값으로 생성 및 복사 버튼 기능 구현

--- jarviser_frontend/src/pages/CreateMeeting.jsx
+++ jarviser_frontend/src/pages/CreateMeeting.jsx
@@ -1,27 +1,54 @@
-// import {useState} from "react";
-// import VideoRoomComponent from "../components/openvidu/VideoRoomComponentid";
-// // import registerServiceWorker from "../registerServiceWorker";
-// import ReactDOM from "react-dom/client";
-// console.log("방 생성하기 호출!");
-
-// function CreateMeeting() {
-//   return <VideoRoomComponent />;
-// }
-// // registerServiceWorker();
-
-// export default CreateMeeting;
-
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import VideoRoomComponent from "../components/openvidu/VideoRoomComponent"; // 경로는 실제 파일 위치에 따라 수정하세요.
+
+const generateInitialSessionName = () => {
+  const array = new Uint32Array(1);
+  window.crypto.getRandomValues(array);
+  const randomValue = array[0];
+  const encoder = new TextEncoder();
+  const data = encoder.encode(randomValue.toString());
+
+  return window.crypto.subtle.digest("SHA-256", data).then((hashBuffer) => {
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+    return hashHex;
+  });
+};
 
 const CreateMeeting = () => {
   const [userName, setUserName] = useState("1234");
   const [sessionName, setSessionName] = useState("My Room");
   const [showVideoRoom, setShowVideoRoom] = useState(false);
 
+  useEffect(() => {
+    async function initializeSessionName() {
+      const generatedName = await generateInitialSessionName();
+      setSessionName(generatedName);
+    }
+
+    initializeSessionName();
+  }, []);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    setShowVideoRoom(true); // 폼이 제출되었으므로 VideoRoomComponent를 보여줍니다.
+    setShowVideoRoom(true);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(sessionName)
+      .then(() => {
+        alert("Session Name copied to clipboard!");
+      })
+      .catch((err) => {
+        alert("Failed to copy!");
+      });
+  };
+
+  const handleSessionNameChange = (event) => {
+    setSessionName(event.target.value);
   };
 
   return (
@@ -40,14 +67,16 @@ const CreateMeeting = () => {
           <input
             type="text"
             value={sessionName}
-            onChange={(event) => setSessionName(event.target.value)}
+            onChange={handleSessionNameChange}
           />
+          <button type="button" onClick={handleCopy}>
+            Copy
+          </button>
         </label>
         <input type="submit" value="Submit" />
       </form>
       {showVideoRoom && (
         <VideoRoomComponent userName={userName} sessionName={sessionName} />
-        // <VideoRoomComponent />
       )}
     </div>
   );
