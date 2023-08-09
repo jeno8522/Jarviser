a5f7758c4924172882b081faee7a9261df060347
wooseok <wooseok777777@gmail.com>
2023-08-09 17:21:50 +0900

    feat : audioMessage 더미 데이터 추가, audioMessage MeetingId로 meeintgId 회의 내용 추가 api

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/domain/Meeting.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/domain/Meeting.java
@@ -42,9 +42,6 @@ public class Meeting implements Serializable {
     @OneToMany(mappedBy = "meeting")
     private final List<AudioMessage> audioMessages = new ArrayList<>();
 
-    @OneToMany(mappedBy = "meeting")
-    private final List<ChatMessage> chatMessages = new ArrayList<>();
-
     @OneToOne(mappedBy = "meeting")
     private Report report;
 
@@ -58,6 +55,12 @@ public class Meeting implements Serializable {
         this.hostId = hostId;
         this.meetingUrl = meetingUrl;
         this.startTime = startTime;
+    }
+
+    //양방향 맵핑 설정
+    public void addAudioMessage(AudioMessage audioMessage) {
+        this.audioMessages.add(audioMessage);
+        audioMessage.setMeeting(this); // 연관 관계의 주인이 AudioMessage이므로 이 부분이 중요
     }
 }
 
---
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/dto/ResponseAudioMessage.java
@@ -0,0 +1,10 @@
+package com.ssafy.jarviser.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseAudioMessage {
+    private String name;
+    private String content;
+    private String length;
+}
