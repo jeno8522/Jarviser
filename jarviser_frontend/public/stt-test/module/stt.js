907dcf0ccac7d0c20d1965c3218ba006578ec23b
jeno8522 <jeno8522@gmail.com>
2023-08-10 17:38:47 +0900

    feat: 현웅이 STT 이식시도1

--- jarviser_frontend/public/stt-test/module/stt.js
+++ jarviser_frontend/public/stt-test/module/stt.js
@@ -1,6 +1,6 @@
 document.getElementById("vad_start").addEventListener("click", () => {
   navigator.mediaDevices
-    .getUserMedia({ audio: true })
+    .getUserMedia({audio: true})
     .then((stream) => startVAD(stream)) // Pass the 'stream' to the startVAD function
     .catch((err) => console.log("getUserMedia() failed: ", err));
 });
@@ -29,13 +29,14 @@ function startVAD(stream) {
     },
   };
   vad = new window.VAD(options); // Initialize VAD with the correct options
+  console.log("vad.start는 여기요여기 == ", vad.start);
   vad.start = true; // Start VAD
   console.log("startVAD");
 }
 
 function stopVAD() {
   navigator.mediaDevices
-    .getUserMedia({ audio: true, video: false })
+    .getUserMedia({audio: true, video: false})
     .then(function (stream) {
       var audioTrack = stream.getAudioTracks()[0];
       audioTrack.enabled = false;
@@ -59,7 +60,7 @@ function startAudio(stream) {
     });
 
     mediaRecorder.addEventListener("stop", function () {
-      let blob = new Blob(recordedChunks, { type: "audio/wav" });
+      let blob = new Blob(recordedChunks, {type: "audio/wav"});
       recordedChunks = [];
       sendAudio(blob);
       index++;
@@ -78,7 +79,7 @@ function stopAudio() {
 
 async function sendAudio(blob) {
   try {
-    const url = "http://localhost:8081/meeting/transcript"; //backend api url 넣기
+    const url = "http://localhost:8081/meeting/transcript";
     const formData = new FormData();
     const testID = 3; //임시로 넣은 testID
     formData.append("file", blob, "audio" + index + ".wav");
@@ -93,6 +94,13 @@ async function sendAudio(blob) {
     }
     const data = await response.json();
     console.log(data.text);
+
+    // STT 결과를 <div id="stt-chatting"></div>에 추가하는 부분
+    const chatDiv = document.getElementById("stt-chatting");
+    const messageDiv = document.createElement("div");
+    messageDiv.className = "stt-message"; // CSS 클래스 추가 (필요시 스타일링을 위해)
+    messageDiv.textContent = data.text;
+    chatDiv.appendChild(messageDiv);
   } catch (error) {
     console.error("Error sending audio", error);
   }
