d00bfcb535be281cb1badeb101cea5b54d81af8c
taehyun <taehyun0121@naver.com>
2023-08-13 23:40:54 +0900

    Sign up UI 및 Sign in UI 생성

--- jarviser_frontend/src/pages/Login.jsx
+++ jarviser_frontend/src/pages/Login.jsx
@@ -1,33 +1,30 @@
-import { useState } from "react";
+import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useAccessToken from "../components/useAccessToken";
+import jarviserImg from "../assets/images/Jarviser_logo.jpg"; // 로고 이미지 경로
+
 function Login() {
   const navigate = useNavigate();
   const { accessToken } = useAccessToken();
 
-  useEffect(() => {
-    if (accessToken) {
-      navigate("/usermain");
-    }
-  }, [accessToken, navigate]);
+  if (accessToken) {
+    navigate("/usermain");
+  }
+
   const onSubmit = async (data) => {
     try {
       await new Promise((r) => setTimeout(r, 1000));
-      const response = await axios.post(
-        "http://localhost:8081/user/login",
-        data
-      );
+      const response = await axios.post("http://localhost:8081/user/login", data);
       const accessToken = response.data["access-token"];
       localStorage.setItem("access-token", accessToken);
       navigate("/userMain");
     } catch (error) {
       console.error("로그인 요청 에러:", error);
     }
-    alert(JSON.stringify(data));
   };
+
   const {
     register,
     handleSubmit,
@@ -35,55 +32,156 @@ function Login() {
   } = useForm();
 
   return (
-    <>
-      <div>
-        <h1>로그인</h1>
-      </div>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">이메일</label>
-        <input
-          id="email"
-          type="email"
-          placeholder="아이디를 입력해주세요."
-          aria-invalid={
-            isSubmitted ? (errors.email ? "true" : "false") : undefined
-          }
-          {...register("email", {
-            required: "이메일은 필수 입력입니다.",
-            pattern: {
-              value: /\S+@\S+\.\S+/,
-              message: "이메일 형식에 맞지 않습니다.",
-            },
-          })}
-        />
-        {errors.email && <small role="alert">{errors.email.message}</small>}
-        <br />
-        <label htmlFor="password">비밀번호</label>
-        <input
-          id="password"
-          type="password"
-          placeholder="비밀번호를 입력해주세요."
-          aria-invalid={
-            isSubmitted ? (errors.password ? "true" : "false") : undefined
-          }
-          {...register("password", {
-            required: "비밀번호는 필수 입력입니다.",
-            minLength: {
-              value: 8,
-              message: "8자리 이상 비밀번호를 사용하세요.",
-            },
-          })}
-        />
-        {errors.password && (
-          <small role="alert">{errors.password.message}</small>
-        )}
-        <br />
-        <button type="submit" disabled={isSubmitting}>
-          로그인
-        </button>
-      </form>
-    </>
+    <Whole>
+      <ImageLogo src={jarviserImg} alt="Jarviser Logo" />
+      <RightColumn>
+        <LoginHeadLine>
+          <h1>Sign in</h1>
+        </LoginHeadLine>
+        <LoginForm onSubmit={handleSubmit(onSubmit)}>
+          <LoginLabel htmlFor="email">이메일</LoginLabel>
+          <LoginField
+            id="email"
+            type="email"
+            placeholder="아이디를 입력해주세요."
+            aria-invalid={isSubmitted ? (errors.email ? "true" : "false") : undefined}
+            {...register("email", {
+              required: "이메일은 필수 입력입니다.",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "이메일 형식에 맞지 않습니다.",
+              },
+            })}
+          />
+          {errors.email && <small role="alert">{errors.email.message}</small>}
+          <LoginLabel htmlFor="password">비밀번호</LoginLabel>
+          <LoginField
+            id="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            aria-invalid={isSubmitted ? (errors.password ? "true" : "false") : undefined}
+            {...register("password", {
+              required: "비밀번호는 필수 입력입니다.",
+              minLength: {
+                value: 8,
+                message: "8자리 이상 비밀번호를 사용하세요.",
+              },
+            })}
+          />
+          {errors.password && <small role="alert">{errors.password.message}</small>}
+          <LoginButton type="submit" disabled={isSubmitting}>
+            로그인
+          </LoginButton>
+        </LoginForm>
+      </RightColumn>
+    </Whole>
   );
 }
+
+// Styled Components (스타일 부분)
+
+const Whole = styled.div`
+  display: flex;
+  width: 100%;
+  height: 60vh;
+  flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageLogo = styled.img`
+  width: 50%;
+  height: 90%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  margin-left: 20px;
+  margin-right: -20px;
+`;
+
+const RightColumn = styled.div`
+  display: flex;
+  width: 50%;
+  height: 80%;
+  padding: 40px;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  box-sizing: border-box;
+`;
+
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 40%;
+  gap: 8px;
+`;
+
+const LoginLabel = styled.label`
+  align-self: stretch;
+  color: var(--cool-gray-70, #4d5358);
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 4px;
+  letter-spacing: 0.5px;
+  text-transform: uppercase;
+`;
+
+const LoginField = styled.input`
+  width: 200%;
+  height: 15px;
+  padding: 6px;
+  border: 1px solid var(--cool-gray-30, #C1C7CD);
+  border-radius: 4px;
+  background: var(--cool-gray-10, #F2F4F8);
+  color: var(--cool-gray-60, #697077);
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  transition: border 0.3s ease;
+
+  &:focus {
+    border: 1px solid var(--primary-60, #0F62FE);
+  }
+
+  &::placeholder {
+    color: var(--cool-gray-30, #C1C7CD);
+  }
+`;
+
+const LoginButton = styled.button`
+  width: 100%;
+  height: 48px;
+  padding: 12px;
+  border: none;
+  border-radius: 4px;
+  background: var(--primary-60, #0F62FE);
+  color: white;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background 0.3s ease;
+
+  &:hover {
+    background: var(--primary-50, #0043CE);
+  }
+
+  &:disabled {
+    background: var(--cool-gray-30, #C1C7CD);
+    cursor: not-allowed;
+  }
+`;
+
+const LoginHeadLine = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px;
+  font-weight: 500;
+  color: var(--primary-60, #0F62FE);
+  text-align: left;
+  letter-spacing: -1px;
+  margin-bottom: 16px;
+`;
+
 export default Login;
--- jarviser_frontend/src/pages/Signup.jsx
+++ jarviser_frontend/src/pages/Signup.jsx
@@ -1,92 +1,208 @@
-import { useState } from "react";
+import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import jarviserImg from "../assets/images/Jarviser_logo.jpg";
 
 function Signup() {
   const navigate = useNavigate();
-
-  const onSubmit = async (data) => {
-    await new Promise((r) => setTimeout(r, 1000));
-    axios.post("http://localhost:8081/user/signup", data);
-
-    alert(JSON.stringify(data));
-    navigate("/login");
-  };
   const {
     register,
     handleSubmit,
     formState: { isSubmitting, isSubmitted, errors },
+    watch,
   } = useForm();
 
-  return (
-    <>
-      <div>
-        <h1>회원가입</h1>
-      </div>
+  const password = watch("password"); // 비밀번호 필드를 확인
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">이메일</label>
-        <input
-          id="email"
-          type="email"
-          placeholder="아이디를 입력해주세요."
-          aria-invalid={
-            isSubmitted ? (errors.email ? "true" : "false") : undefined
-          }
-          {...register("email", {
-            required: "이메일은 필수 입력입니다.",
-            pattern: {
-              value: /\S+@\S+\.\S+/,
-              message: "이메일 형식에 맞지 않습니다.",
-            },
-          })}
-        />
-        {errors.email && <small role="alert">{errors.email.message}</small>}
-        <br />
-        <label htmlFor="password">비밀번호</label>
-        <input
-          id="password"
-          type="password"
-          placeholder="비밀번호를 입력해주세요."
-          aria-invalid={
-            isSubmitted ? (errors.password ? "true" : "false") : undefined
-          }
-          {...register("password", {
-            required: "비밀번호는 필수 입력입니다.",
-            minLength: {
-              value: 8,
-              message: "8자리 이상 비밀번호를 사용하세요.",
-            },
-          })}
-        />
-        {errors.password && (
-          <small role="alert">{errors.password.message}</small>
-        )}
-        <br />
-        <label htmlFor="name">이름</label>
-        <input
-          id="name"
-          type="text"
-          placeholder="이름을 입력해주세요."
-          aria-invalid={
-            isSubmitted ? (errors.name ? "true" : "false") : undefined
-          }
-          {...register("name", {
-            required: "이름은 필수 입력입니다.",
-            minLength: {
-              value: 2,
-              message: "2자리 이상 이름을 입력하세요.",
-            },
-          })}
-        />
-        {errors.name && <small role="alert">{errors.name.message}</small>}
-        <br />
-        <button type="submit" disabled={isSubmitting}>
-          회원가입
-        </button>
-      </form>
-    </>
+  const onSubmit = async (data) => {
+    await new Promise((r) => setTimeout(r, 1000));
+    axios.post("http://localhost:8081/user/signup", data);
+    alert(JSON.stringify(data));
+    navigate("/login");
+  };
+
+  return (
+    <Whole>
+      <ImageLogo src={jarviserImg} alt="Jarviser Logo" />
+      <RightColumn>
+        <SignupHeadLine>
+          <h1>Sign up</h1>
+        </SignupHeadLine>
+        <SignupForm onSubmit={handleSubmit(onSubmit)}>
+          <SignupLabel htmlFor="email">이메일</SignupLabel>
+          <SignupField
+            id="email"
+            type="email"
+            placeholder="아이디를 입력해주세요."
+            aria-invalid={isSubmitted ? (errors.email ? "true" : "false") : undefined}
+            {...register("email", {
+              required: "이메일은 필수 입력입니다.",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "이메일 형식에 맞지 않습니다.",
+              },
+            })}
+          />
+          {errors.email && <small role="alert">{errors.email.message}</small>}
+          <SignupLabel htmlFor="password">비밀번호</SignupLabel>
+          <SignupField
+            id="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            aria-invalid={isSubmitted ? (errors.password ? "true" : "false") : undefined}
+            {...register("password", {
+              required: "비밀번호는 필수 입력입니다.",
+              minLength: {
+                value: 8,
+                message: "8자리 이상 비밀번호를 사용하세요.",
+              },
+            })}
+          />
+          {errors.password && <small role="alert">{errors.password.message}</small>}
+          <SignupLabel htmlFor="passwordConfirm">비밀번호 확인</SignupLabel>
+          <SignupField
+            id="passwordConfirm"
+            type="password"
+            placeholder="비밀번호를 다시 입력해주세요."
+            aria-invalid={isSubmitted ? (errors.passwordConfirm ? "true" : "false") : undefined}
+            {...register("passwordConfirm", {
+              required: "비밀번호 확인은 필수 입력입니다.",
+              validate: (value) => value === password || "비밀번호가 일치하지 않습니다.",
+            })}
+          />
+          {errors.passwordConfirm && (
+            <small role="alert">{errors.passwordConfirm.message}</small>
+          )}
+          <SignupLabel htmlFor="name">이름</SignupLabel>
+          <SignupField
+            id="name"
+            type="text"
+            placeholder="이름을 입력해주세요."
+            aria-invalid={isSubmitted ? (errors.name ? "true" : "false") : undefined}
+            {...register("name", {
+              required: "이름은 필수 입력입니다.",
+              minLength: {
+                value: 2,
+                message: "2자리 이상 이름을 입력하세요.",
+              },
+            })}
+          />
+          {errors.name && <small role="alert">{errors.name.message}</small>}
+          <RegisterButton type="submit" disabled={isSubmitting}>
+            회원가입
+          </RegisterButton>
+        </SignupForm>
+      </RightColumn>
+    </Whole>
   );
 }
+const Whole = styled.div`
+  display: flex;
+  width: 100%;
+  height: 60vh;
+  flex-shrink: 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageLogo = styled.img`
+  width: 50%;
+  height: 90%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  margin-left: 20px;
+  margin-right: -20px;
+`;
+
+const RightColumn = styled.div`
+  display: flex;
+  width: 50%;
+  height: 80%;
+  padding: 40px;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  box-sizing: border-box;
+`;
+
+
+
+const SignupForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 40%;
+  gap: 8px; // 각 필드와 레이블 사이의 간격
+`;
+
+const SignupLabel = styled.label`
+align-self: stretch;
+color: var(--cool-gray-70, #4d5358); // 글씨 색상 변경
+font-family: 'Roboto', sans-serif;
+font-size: 16px; // 글자 크기 조정
+font-weight: 500; // 글씨 두께 조정
+margin-bottom: 4px; // 아래쪽 마진 추가
+letter-spacing: 0.5px; // 글자 간격 조정
+text-transform: uppercase; // 대문자 변환
+`;
+
+const SignupField = styled.input`
+  width: 200%; // 전체 너비
+  height: 15px;
+  padding: 6px;
+  border: 1px solid var(--cool-gray-30, #C1C7CD);
+  border-radius: 4px; // 모서리 둥글게
+  background: var(--cool-gray-10, #F2F4F8);
+  color: var(--cool-gray-60, #697077);
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  transition: border 0.3s ease; // 효과 추가
+
+  &:focus {
+    border: 1px solid var(--primary-60, #0F62FE); // 포커스시 테두리 색상 변경
+  }
+
+  &::placeholder {
+    color: var(--cool-gray-30, #C1C7CD); // 플레이스홀더 색상 변경
+  }
+`;
+
+const RegisterButton = styled.button`
+  width: 100%; // 전체 너비
+  height: 48px;
+  padding: 12px;
+  border: none; // 테두리 제거
+  border-radius: 4px; // 모서리 둥글게
+  background: var(--primary-60, #0F62FE);
+  color: white; // 글씨색 변경
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  font-weight: 500; // 글씨 두껍게
+  cursor: pointer; // 마우스 커서 변경
+  transition: background 0.3s ease; // 효과 추가
+
+  &:hover {
+    background: var(--primary-50, #0043CE); // 마우스 오버시 색상 변경
+  }
+
+  &:disabled {
+    background: var(--cool-gray-30, #C1C7CD); // 비활성화시 색상 변경
+    cursor: not-allowed; // 마우스 커서 변경
+  }
+`;
+
+// 나머지 코드는 이전과 동일
+
+
+const SignupHeadLine = styled.h1`
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px; // 글자 크기 조정
+  font-weight: 500; // 글씨 두껍게
+  color: var(--primary-60, #0F62FE); // 글씨 색상 변경
+  text-align: left; // 왼쪽 정렬
+  letter-spacing: -1px; // 글자 간격 조정
+  margin-bottom: 0px; // Header와 Form 간격 조정
+`;
 export default Signup;
