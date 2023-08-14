f09368187d42630e5d491ead6c31738027c02a39
wooseok <wooseok777777@gmail.com>
2023-08-14 15:57:06 +0900

    feat : participant 중복 처리 로직 추가

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/repository/ParticipantRepository.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/repository/ParticipantRepository.java
@@ -5,4 +5,6 @@ import com.ssafy.jarviser.domain.Participant;
 public interface ParticipantRepository {
     //참여자 등록
     void joinParticipant(Participant participant);
+
+    Participant findParticipant(long meetingId,long userId);
 }
