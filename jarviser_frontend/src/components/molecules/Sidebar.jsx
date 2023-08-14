83610266795e1950f0917fce363a9eab271ec8a8
kimtaehyun <taehyun01212@gmail.com>
2023-08-14 17:43:44 +0900

    feat: side bar 크기 수정 및 텍스트 추가

--- jarviser_frontend/src/components/molecules/Sidebar.jsx
+++ jarviser_frontend/src/components/molecules/Sidebar.jsx
@@ -12,13 +12,14 @@
     import Document from "../../logo/document.png";
     import LogoutIcon from "../../logo/logout.png";
     import MainHeader from "./MainHeader";
+    
     function Sidebar() {
       const navigate = useNavigate();
       const menus = [
-        { name: "회원정보", path: "/myPage" },
-        { name: "캘린더", path: "/myCalendar" },
-        { name: "회의록", path: "/myReport" },
-        { name: "로그아웃", path: "/" },
+        { name: "회원정보", path: "/myPage", icon: User },
+    { name: "캘린더", path: "/myCalendar", icon: Calendar },
+    { name: "회의록", path: "/myReport", icon: Document },
+    { name: "로그아웃", path: "/", icon: LogoutIcon },
       ];
       const Logout = () => {
         localStorage.removeItem("access-token");
@@ -27,19 +28,23 @@
       return (
         <>
           <SidebarContianer>
-            <a href="/myPage">
-              <LogoImage src={User} alt="User" />
-            </a>
-            <a href="/myCalendar">
-              <LogoImage src={Calendar} alt="MyCalendar" />
-            </a>
-            <a href="/myReport">
-              <LogoImage src={Document} alt="Document" />
-            </a>
-            <a href="/">
-              <LogoImage src={LogoutIcon} alt="Logout" onClick={Logout} />
-            </a>
-          </SidebarContianer>
+      <MenuItem href="/myPage">
+        <LogoImage src={User} alt="User" />
+        <MenuText>회원정보</MenuText>
+      </MenuItem>
+      <MenuItem href="/myCalendar">
+        <LogoImage src={Calendar} alt="MyCalendar" />
+        <MenuText>캘린더</MenuText>
+      </MenuItem>
+      <MenuItem href="/myReport">
+        <LogoImage src={Document} alt="Document" />
+        <MenuText>회의록</MenuText>
+      </MenuItem>
+      <MenuItem href="/" onClick={Logout}>
+        <LogoImage src={LogoutIcon} alt="Logout" />
+        <MenuText>로그아웃</MenuText>
+      </MenuItem>
+    </SidebarContianer>
         </>
       );
     }
@@ -50,7 +55,7 @@
       height: 80px;
       flex-shrink: 0;
       margin-left: 20px;
-      margin: 40px;
+      margin: 0px;
     `;
     
     const SidebarContianer = styled.div`
@@ -60,4 +65,21 @@
       height: 104%;
       flex-shrink: 0;
       background-color: #cae1fd;
+      
     `;
+
+    const MenuItem = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px;
+  text-decoration: none; // 링크 밑줄 제거
+  color: #333; // 글자 색상
+`;
+
+const MenuText = styled.span`
+  
+  font-size: 14px;
+  font-weight: 500;
+  margin-top: 2px; // 이미지와의 간격
+`;
