7e53059faec8d95dfd04c92984900132147dbe4c
wooseok <wooseok777777@gmail.com>
2023-08-14 15:40:21 +0900

    feat :

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/controller/MeetingController.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/controller/MeetingController.java
@@ -39,54 +39,11 @@ public class MeetingController {
     private final SimpMessagingTemplate messagingTemplate;
     private final AESEncryptionUtil aesEncryptionUtil;
 
-    @PostMapping(value = "/transcript", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Map<String, String>> transcript(@RequestParam("file") MultipartFile file, Long meetingId) throws IOException {
-        Map<String, String> resultMap = new HashMap<>();
-        HttpStatus status = null;
-        String filePath = "audio/" + file.getOriginalFilename();
-        log.debug(filePath);
-
-        /*
-        TODO: 추후 MultiPartFile을 File로 즉각 변환해본 후 성능 테스트해보기
-               비동기처리를 하고 싶을 때 파일 저장부분을 따로 분리해서 해당 함수 위에 @Async
-               그리고 테스트를 해보고싶으면 해당 함수 내에서 Thread.sleep
-         */
-        try (
-                FileOutputStream fos = new FileOutputStream(filePath);
-                // 파일 저장할 경로 + 파일명을 파라미터로 넣고 fileOutputStream 객체 생성하고
-                InputStream is = file.getInputStream();) {
-
-            int readCount = 0;
-            byte[] buffer = new byte[1024];
-
-            while ((readCount = is.read(buffer)) != -1) {
-                //  파일에서 가져온 fileInputStream을 설정한 크기 (1024byte) 만큼 읽고
-                fos.write(buffer, 0, readCount);
-                // 위에서 생성한 fileOutputStream 객체에 출력하기를 반복한다
-            }
-        } catch (Exception ex) {
-            throw new RuntimeException("file Save Error");
-        }
-        try {
-            String textResponse = openAIService.whisperAPICall(filePath).block();
-            assert textResponse != null;
-            messagingTemplate.convertAndSend("/topic/meeting/" + meetingId, textResponse);
-
-            resultMap.put("text", textResponse);
-        } catch (Exception e) {
-            log.error("텍스트 보내기 실패 : {}", e);
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-
-        return new ResponseEntity<>(resultMap, status.OK);
-    }
-
-
     //미팅생성
-    @PostMapping("/create")
+    @PostMapping("/create/{meetingName}")
     public ResponseEntity<Map<String, Object>> createMeeting(
             @RequestHeader("Authorization") String token,
-            @RequestBody String meetingName) {
+            @PathVariable String meetingName) {
         log.debug("CreateMeeting............................create meetingName:" + meetingName);
 
         Map<String, Object> responseMap = new HashMap<>();
