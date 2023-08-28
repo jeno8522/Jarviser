f9f220c4b0dfea348a9c6741134b59999d303f62
taehyun <taehyun0121@naver.com>
2023-08-28 23:05:34 +0900

    setting: Caching 관련 세팅 적용

--- jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/JarviserApplication.java
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/JarviserApplication.java
@@ -3,10 +3,12 @@ package com.ssafy.jarviser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableCaching
 public class JarviserApplication {
 
 	public static void main(String[] args) {
