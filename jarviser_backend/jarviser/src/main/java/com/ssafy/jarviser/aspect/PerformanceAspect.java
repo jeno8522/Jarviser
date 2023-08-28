d7bed14942e8a6767412a38a4fa75511926a3a09
taehyun <taehyun0121@naver.com>
2023-08-28 23:24:16 +0900

    refactor: AOP를 적용하여 성능 측정 관심사 분리

---
+++ jarviser_backend/jarviser/src/main/java/com/ssafy/jarviser/aspect/PerformanceAspect.java
@@ -0,0 +1,23 @@
+package com.ssafy.jarviser.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class PerformanceAspect {
+
+    @Around("execution(* com.ssafy.jarviser.service.MeetingServiceImp.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+
+        log.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
